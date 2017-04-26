632c5084ab0ccc5c7ff2b2e2acd32646d8d3606b
Dysoch <frits@devreugd.nl>
2017-04-26 13:13:14 +0200

    small fixes

--- DyWorld_0.2.0/script/migration.lua
+++ DyWorld_0.2.0/script/migration.lua
@@ -2,7 +2,7 @@ module("migration", package.seeall)
 
 function Migrate_To_Next_Version()
 	if not global.dyworld.Version then
-		global.dyworld.Version = = "0.2.0" 
+		global.dyworld.Version = "0.2.0" 
 		for k,v in pairs(global.players) do
 			v.Alive = true
 		end

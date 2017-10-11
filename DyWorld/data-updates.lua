ed6a875c86a43fe12a4ca24dea87dfb56a5d8ad5
Dysoch <frits@devreugd.nl>
2017-10-11 12:01:45 +0200

    fluid void recipe

---
+++ DyWorld/data-updates.lua
@@ -0,0 +1,28 @@
+require "data/prefix"
+
+for k,v in pairs(data.raw.fluid) do
+data:extend(
+{	
+  {
+    type = "recipe",
+    name = dy.."void-"..v.name,
+	icons = {
+	  {
+		icon = v.icon,
+	  },
+	  {
+		icon = dyworld_path_icon_temp,
+	  },
+	},
+    energy_required = 1,
+	category = "crafting-with-fluid",
+    enabled = true,
+    ingredients =
+    {
+      {type = "fluid", name = v.name, amount = 50},
+    },
+    results = {{type = "fluid", name = v.name, amount = 0}},
+  },
+}
+)
+end	

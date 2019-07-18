4849f61ae6aa3350ea323ffdbab74976351e1813
Dysoch <frits@devreugd.nl>
2019-07-18 18:41:26 +0200

    Part 4 item group

---
+++ DyWorld/data/core/item-groups/inserters.lua
@@ -0,0 +1,76 @@
+require "data/core/functions/prefix"
+
+local Data_Table_Item_Group = {
+	{
+		Name = "inserters",
+		Type = "main",
+		Icon = "inserters.png"
+	},
+	{
+		Name = "inserter-1-simple",
+		Type = "sub",
+		Main = "inserters"
+	},
+	{
+		Name = "inserter-2-basic",
+		Type = "sub",
+		Main = "inserters"
+	},
+	{
+		Name = "inserter-2-filter",
+		Type = "sub",
+		Main = "inserters"
+	},
+	{
+		Name = "inserter-2-long",
+		Type = "sub",
+		Main = "inserters"
+	},
+	{
+		Name = "inserter-3-stack",
+		Type = "sub",
+		Main = "inserters"
+	},
+	{
+		Name = "inserter-3-stack-filter",
+		Type = "sub",
+		Main = "inserters"
+	},
+}
+
+function DyWorld_Item_Main_Group(NAME, ICON)
+  local result =
+  {
+    type = "item-group",
+    name = dy..NAME,
+    icon = dyworld_path_item_group..ICON,
+    icon_size = 64,
+    order = "z-"..dy..NAME,
+  }
+  return result
+end
+
+function DyWorld_Item_Sub_Group(NAME, MAIN)
+  local result =
+  {
+    type = "item-subgroup",
+    name = dy..NAME,
+    group = dy..MAIN,
+    order = dy..NAME
+  }
+  return result
+end
+
+for k,v in pairs(Data_Table_Item_Group) do
+	if v.Type == "main" then
+		data:extend(
+			{
+				DyWorld_Item_Main_Group(v.Name, v.Icon)
+			})
+	elseif v.Type == "sub" then
+		data:extend(
+			{
+				DyWorld_Item_Sub_Group(v.Name, v.Main)
+			})
+	end
+end

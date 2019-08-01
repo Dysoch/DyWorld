5d401dcd34d82aaab8a57529579fd651f226add2
Dysoch <frits@devreugd.nl>
2019-08-01 21:35:09 +0200

    dycompa change

---
+++ DyWorld/data/dycompa/changes/dyworld-processing.lua
@@ -0,0 +1,73 @@
+
+
+local Change = {
+	{Old = "assembling-machine-2", New = "copper-assembling-machine"},
+	{Old = "assembling-machine-3", New = "lead-assembling-machine"},
+}
+
+for k,v in pairs(Change) do
+	if v.New then
+		for _, Recipe in pairs(data.raw.recipe) do
+			if Recipe.ingredients then
+				for _, b in pairs(Recipe.ingredients) do
+					if b.name then
+						if b.name == v.Old then
+							b.name = v.New
+						end
+					elseif b[1] == v.Old then
+						b[1] = v.New
+					end
+				end
+			end
+		end
+	end
+end
+
+for k,m in pairs(Change) do
+	for j,n in pairs(data.raw.technology) do
+		if n.effects then
+			for h,b in pairs(n.effects) do
+				if b.recipe == m.Old then
+					table.remove(n.effects, h)
+				end
+			end
+		end
+	end
+	if m.New then
+		if data.raw.item[m.Old] then
+			if data.raw.item[m.Old].flags then
+				table.insert(data.raw.item[m.Old].flags, "hidden")
+			else
+				data.raw.item[m.Old].flags = {"hidden"}
+			end
+		end
+		if data.raw["transport-belt"][m.Old] then
+			if data.raw["transport-belt"][m.Old].next_upgrade then
+				data.raw["transport-belt"][m.Old].next_upgrade = nil
+			end
+		end
+		if data.raw["splitter"][m.Old] then
+			if data.raw["splitter"][m.Old].next_upgrade then
+				data.raw["splitter"][m.Old].next_upgrade = nil
+			end
+		end
+		if data.raw["underground-belt"][m.Old] then
+			if data.raw["underground-belt"][m.Old].next_upgrade then
+				data.raw["underground-belt"][m.Old].next_upgrade = nil
+			end
+		end
+		if data.raw["inserter"][m.Old] then
+			if data.raw["inserter"][m.Old].next_upgrade then
+				data.raw["inserter"][m.Old].next_upgrade = nil
+			end
+		end
+		if data.raw.recipe[m.Old] then
+			if data.raw.recipe[m.Old].normal then
+				data.raw.recipe[m.Old].normal.hidden = true
+				data.raw.recipe[m.Old].expensive.hidden = true
+			else
+				data.raw.recipe[m.Old].hidden = true
+			end
+		end
+	end
+end
--- DyWorld/data/dycompa/data.lua
+++ DyWorld/data/dycompa/data.lua
@@ -5,3 +5,7 @@ require("data.dycompa.edits.item-group-change")
 if settings.startup["DyWorld_Logistics"].value then
 	require("data.dycompa.changes.dyworld-logistic")
 end
+
+if settings.startup["DyWorld_Processing"].value then
+	require("data.dycompa.changes.dyworld-processing")
+end

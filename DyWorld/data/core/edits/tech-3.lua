89dc3cb7e327df96b6ea1de15a49e763b053c30a
Dysoch <frits@devreugd.nl>
2019-08-02 18:19:59 +0200

    Update tech-3.lua

--- DyWorld/data/core/edits/tech-3.lua
+++ DyWorld/data/core/edits/tech-3.lua
@@ -13,13 +13,16 @@ local Tech_PreReq = {
 	["automation-5"] = {"arditium-processing"},
 	["automation-6"] = {"tungsten-processing", "titanium-processing"},
 	["automation-9"] = {"neutronium-processing"},
-	["metallurgy-1"] = {"steel-processing"},
-	["metallurgy-2"] = {"silver-processing", "nickel-processing", "tin-processing"},
-	["metallurgy-3"] = {"lead-processing", "gold-processing"},
-	["metallurgy-4"] = {"cobalt-processing"},
-	["metallurgy-5"] = {"arditium-processing"},
-	["metallurgy-6"] = {"tungsten-processing", "titanium-processing"},
-	["metallurgy-9"] = {"neutronium-processing"},
+	["metallurgy-1"] = {"steel-processing", "smelting-1"},
+	["metallurgy-2"] = {"silver-processing", "nickel-processing", "tin-processing", "smelting-2"},
+	["metallurgy-3"] = {"lead-processing", "gold-processing", "smelting-3"},
+	["metallurgy-4"] = {"cobalt-processing", "smelting-4"},
+	["metallurgy-5"] = {"arditium-processing", "smelting-5"},
+	["metallurgy-6"] = {"tungsten-processing", "titanium-processing", "smelting-6"},
+	["metallurgy-7"] = {"smelting-7"},
+	["metallurgy-8"] = {"smelting-8"},
+	["metallurgy-9"] = {"neutronium-processing", "smelting-9"},
+	["metallurgy-10"] = {"smelting-10"},
 } 
 
 for k,v in pairs(Tech_PreReq) do

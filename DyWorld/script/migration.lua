42d13dfbd4fc36f9ad9d58fbaf95c76bb80e3cc0
Dysoch <frits@devreugd.nl>
2019-08-06 16:40:48 +0200

    Update migration.lua
    this closes #97

--- DyWorld/script/migration.lua
+++ DyWorld/script/migration.lua
@@ -7,42 +7,23 @@ function Migrate_To_Next_Version()
 			global.dyworld.Research_Done == 0
 			global.stats.research = 0
 			global.dyworld.Migration_Check = true
-			for k,v in pairs(game.players[1].force.technologies) do
-				if Check_Tech(v.name) then
-					if v.researched then
-						v.researched = false
-						v.researched = true
+			if game.players and game.players[1] then
+				for k,v in pairs(game.players[1].force.technologies) do
+					if Check_Tech(v.name) then
+						if v.researched then
+							v.researched = false
+							v.researched = true
+						end
 					end
 				end
 			end
 			global.dyworld.Migration_Check = false
 		global.dyworld.Version = "0.8.0" 
 		PlayerPrint({"dyworld.new-version", (global.dyworld.Version)})
+		PlayerPrint("Please start over! DyWorld has changed a lot since version 0.7.x, and certain things are no longer working. No migration will happen from 0.7.x to 0.8, since that is to much edits that might corrupt savegame. Please start a new game!")
 		--@todo add migration message
 		--@body A message that says to start over with a new game since DyWorld changed a lot
 	end
-	--[[if global.dyworld.Version == "0.7.5" or global.dyworld.Version == "0.7.81" then
-			remote.call("silo_script", "set_show_launched_without_satellite", false)
-			remote.call("silo_script", "set_finish_on_launch", false)
-			remote.call("silo_script", "remove_tracked_item", "satellite")
-			remote.call("silo_script", "add_tracked_item", "dyworld-dtx-001-spaceship")
-			-- This part below is done every migration!
-			global.dyworld.Max_Research = Research_Calc(),
-			global.dyworld.Research_Done = 0
-			global.stats.research = 0
-			global.dyworld.Migration_Check = true
-			for k,v in pairs(game.players[1].force.technologies) do
-				if Check_Tech(v.name) then
-					if v.researched then
-						v.researched = false
-						v.researched = true
-					end
-				end
-			end
-			global.dyworld.Migration_Check = false
-		global.dyworld.Version = "0.8.0" 
-		PlayerPrint({"dyworld.new-version", (global.dyworld.Version)})
-	end]]
 end
 
 function Migrate_Debug()

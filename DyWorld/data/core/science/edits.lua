188e3354ca90d826d7058cf42fbbc8a815947d0b
Dysoch <frits@devreugd.nl>
2019-08-08 17:22:12 +0200

    science changes
    This closes #207

--- DyWorld/data/core/science/edits.lua
+++ DyWorld/data/core/science/edits.lua
@@ -16,23 +16,23 @@ data.raw.recipe["chemical-science-pack"].category = "crafting-with-fluid"
 data.raw.recipe["chemical-science-pack"].energy_required = 2.5
 data.raw.recipe["chemical-science-pack"].ingredients = {
     {type = "fluid", name = "science-3", amount = 5},
-	{type = "item", name = "steel-plate", amount = 2},
+	{type = "item", name = "cobalt-plate", amount = 2},
 }
 data.raw.recipe["military-science-pack"].category = "crafting-with-fluid"
 data.raw.recipe["military-science-pack"].energy_required = 2.5
 data.raw.recipe["military-science-pack"].ingredients = {
     {type = "fluid", name = "science-4", amount = 5},
-	{type = "item", name = "steel-plate", amount = 2},
+	{type = "item", name = "tungsten-plate", amount = 2},
 }
 data.raw.recipe["production-science-pack"].category = "crafting-with-fluid"
 data.raw.recipe["production-science-pack"].energy_required = 2.5
 data.raw.recipe["production-science-pack"].ingredients = {
     {type = "fluid", name = "science-5", amount = 5},
-	{type = "item", name = "steel-plate", amount = 2},
+	{type = "item", name = "titanium-plate", amount = 2},
 }
 data.raw.recipe["utility-science-pack"].category = "crafting-with-fluid"
 data.raw.recipe["utility-science-pack"].energy_required = 2.5
 data.raw.recipe["utility-science-pack"].ingredients = {
     {type = "fluid", name = "science-6", amount = 5},
-	{type = "item", name = "steel-plate", amount = 2},
+	{type = "item", name = "neutronium-plate", amount = 2},
 }
--- DyWorld/data/core/science/recipes.lua
+++ DyWorld/data/core/science/recipes.lua
@@ -11,7 +11,7 @@ data:extend(
 	enabled = true,
     ingredients =
     {
-      {type = "fluid", name = "water", amount = 10},
+      {type = "fluid", name = "water", amount = 100},
       {type = "fluid", name = "carbon-dioxide", amount = 10},
       {type = "item", name = "wood", amount = 3},
     },
@@ -133,12 +133,11 @@ data:extend(
 })
 
 
--- TODO Add more variantion in recipes and make complexer
 DyWorld_Add_To_Recipe("science-3", "methane", 5, true)	
-DyWorld_Add_To_Recipe("science-3", "sulfur", 4) --TEMP
-DyWorld_Add_To_Recipe("science-3", "coal", 4) --TEMP
+DyWorld_Add_To_Recipe("science-3", "lubricant", 15, true)	
 
 if settings.startup["DyWorld_Extraction"].value then
+	DyWorld_Add_To_Recipe("science-5", "cobalt-electric-mining-drill", 1)	
 else
 	DyWorld_Add_To_Recipe("science-5", "electric-mining-drill", 1)	
 end
@@ -146,25 +145,34 @@ end
 if settings.startup["DyWorld_Logistics"].value then
 	DyWorld_Add_To_Recipe("science-1", "stone-transport-belt", 1)
 	DyWorld_Add_To_Recipe("science-2", "iron-loader", 1)
-	DyWorld_Add_To_Recipe("science-2", "stone-pipe-to-ground", 3)
+	DyWorld_Add_To_Recipe("science-2", "fast-inserter", 1)
+	DyWorld_Add_To_Recipe("science-2", "stone-pump", 1)
+	DyWorld_Add_To_Recipe("science-3", "bronze-splitter", 1)
 else	
 	DyWorld_Add_To_Recipe("science-1", "transport-belt", 1)
 	DyWorld_Add_To_Recipe("science-2", "transport-belt", 3)
 	DyWorld_Add_To_Recipe("science-2", "pipe-to-ground", 2)
+	DyWorld_Add_To_Recipe("science-3", "fast-splitter", 1)
 end
 
 if settings.startup["DyWorld_Power"].value then
-	
+	DyWorld_Add_To_Recipe("science-3", "gold-solar-panel", 1)	
+	DyWorld_Add_To_Recipe("science-6", "tungstvar-solar-panel", 1)	
 else	
-	
+	DyWorld_Add_To_Recipe("science-3", "solar-panel", 1)
+	DyWorld_Add_To_Recipe("science-6", "solar-panel", 5)
 end
 
 if settings.startup["DyWorld_Processing"].value then
 	DyWorld_Add_To_Recipe("science-1", "stone-assembling-machine", 1)
 	DyWorld_Add_To_Recipe("science-5", "copper-centrifuge", 1)	
+	DyWorld_Add_To_Recipe("science-5", "invar-assembling-machine", 1)	
+	DyWorld_Add_To_Recipe("science-6", "electranium-assembling-machine", 1)	
 else	
 	DyWorld_Add_To_Recipe("science-1", "iron-chest", 1)
 	DyWorld_Add_To_Recipe("science-5", "centrifuge", 1)	
+	DyWorld_Add_To_Recipe("science-5", "assembling-machine-3", 1)	
+	DyWorld_Add_To_Recipe("science-6", "assembling-machine-3", 3)	
 end
 
 if settings.startup["DyWorld_Warfare"].value then
@@ -172,5 +180,5 @@ if settings.startup["DyWorld_Warfare"].value then
 	DyWorld_Add_To_Recipe("science-4", "iron-gate", 1)	
 else
 	DyWorld_Add_To_Recipe("science-4", "gun-turret", 1)	
-	DyWorld_Add_To_Recipe("science-4", "iron-wall", 1)	
+	DyWorld_Add_To_Recipe("science-4", "stone-wall", 1)	
 end

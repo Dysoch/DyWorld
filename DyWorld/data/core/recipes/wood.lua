c19737a7f071b56cf7d03a59b3c251e68703c1c8
Dysoch <frits@devreugd.nl>
2019-09-13 18:11:14 +0200

    Fixed wood and acorn issue
    This closes #270

--- DyWorld/data/core/recipes/wood.lua
+++ DyWorld/data/core/recipes/wood.lua
@@ -28,28 +28,6 @@ for k,v in pairs(Data_Table) do
 	end
 end	
 
-for k,v in pairs(data.raw.tree) do
-	if v.minable then
-		v.minable = {
-        mining_particle = "wooden-particle",
-        mining_time = 0.55,
-        results = {
-		  {type = "item", name = "wood", amount_min = 1, amount_max = 7, probability = 0.98},
-		  {type = "item", name = "acorn", amount_min = 1, amount_max = 1, probability = 0.10},
-		},
-      }
-	else
-		v.minable = {
-        mining_particle = "wooden-particle",
-        mining_time = 0.55,
-        results = {
-		  {type = "item", name = "wood", amount_min = 1, amount_max = 7, probability = 0.98},
-		  {type = "item", name = "acorn", amount_min = 1, amount_max = 1, probability = 0.10},
-		},
-      }
-	end
-end
-
 local INSERT = {type = "fluid", name = "water", amount = 250, minimum_temperature = 10, maximum_temperature = 35}
 table.insert(data.raw.recipe["wood"].normal.ingredients, INSERT)
 local INSERT = {type = "fluid", name = "water", amount = 1250, minimum_temperature = 10, maximum_temperature = 25}

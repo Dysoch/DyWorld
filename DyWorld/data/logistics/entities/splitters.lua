745ae638c958b6f61f589d88fe68f83d93f8d749
Dysoch <Dysoch@users.noreply.github.com>
2019-09-19 21:13:56 +0200

    Update splitters.lua

--- DyWorld/data/logistics/entities/splitters.lua
+++ DyWorld/data/logistics/entities/splitters.lua
@@ -44,7 +44,7 @@ if v.DyWorld and v.DyWorld.Entity and v.DyWorld.Entity.Belt then
 	DyWorld_Prototype_Recipe.expensive = {}
 	DyWorld_Prototype_Recipe.normal.ingredients = {{type = "item", name = v.DyWorld.Name.."-transport-belt", amount = 2}}
 	DyWorld_Prototype_Recipe.normal.result = v.DyWorld.Name.."-splitter"
-	DyWorld_Prototype_Recipe.expensive.ingredients = {{type = "item", name = v.DyWorld.Name.."-transport-belt", amount = 10}}
+	DyWorld_Prototype_Recipe.expensive.ingredients = {{type = "item", name = v.DyWorld.Name.."-transport-belt", amount = Expensive_Check(2)}}
 	DyWorld_Prototype_Recipe.expensive.result = v.DyWorld.Name.."-splitter"
 	DyWorld_Prototype_Recipe.ingredients = nil
 	DyWorld_Prototype_Recipe.energy_required = 0

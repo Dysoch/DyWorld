8295fadb759dd8e5bddbd84d66ebd62e9e8f03c4
Dysoch <Dysoch@users.noreply.github.com>
2019-09-19 21:13:17 +0200

    Update loaders.lua

--- DyWorld/data/logistics/entities/loaders.lua
+++ DyWorld/data/logistics/entities/loaders.lua
@@ -38,7 +38,7 @@ if v.DyWorld and v.DyWorld.Entity and v.DyWorld.Entity.Belt then
 	DyWorld_Prototype_Recipe.expensive = {}
 	DyWorld_Prototype_Recipe.normal.ingredients = {{type = "item", name = v.DyWorld.Name.."-transport-belt", amount = 2}}
 	DyWorld_Prototype_Recipe.normal.result = v.DyWorld.Name.."-loader"
-	DyWorld_Prototype_Recipe.expensive.ingredients = {{type = "item", name = v.DyWorld.Name.."-transport-belt", amount = 10}}
+	DyWorld_Prototype_Recipe.expensive.ingredients = {{type = "item", name = v.DyWorld.Name.."-transport-belt", amount = Expensive_Check(2)}}
 	DyWorld_Prototype_Recipe.expensive.result = v.DyWorld.Name.."-loader"
 	DyWorld_Prototype_Recipe.ingredients = nil
 	DyWorld_Prototype_Recipe.localised_name = {"looped-name.belt-4", {"looped-name."..v.DyWorld.Name}}

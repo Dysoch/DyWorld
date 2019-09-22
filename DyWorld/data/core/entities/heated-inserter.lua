6bad159232e280bd435e568f425c0a40ef81a299
Dysoch <frits@devreugd.nl>
2019-09-22 10:41:44 +0200

    Long handed heated inserter
    this closes #245
    as i forgot to do it in last commit

--- DyWorld/data/core/entities/heated-inserter.lua
+++ DyWorld/data/core/entities/heated-inserter.lua
@@ -26,11 +26,12 @@ data:extend(
     energy_source =
     {
       type = "heat",
+	  default_temperature = 15,
       max_temperature = 5000,
-      specific_heat = "1MJ",
-      max_transfer = "2GW",
       min_working_temperature = 250,
       minimum_glow_temperature = 100,
+      specific_heat = "1MJ",
+      max_transfer = "2GW",
       connections =
       {
         {
@@ -201,6 +202,204 @@ data:extend(
     default_stack_control_input_signal = inserter_default_stack_control_input_signal
   },
   {
+    type = "inserter",
+    name = "heated-long-inserter",
+    icon = "__base__/graphics/icons/long-handed-inserter.png",
+    icon_size = 32,
+    flags = {"placeable-neutral", "placeable-player", "player-creation"},
+    minable = {mining_time = 0.1, result = "heated-long-inserter"},
+    max_health = 160,
+    corpse = "long-handed-inserter-remnants",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 90
+      }
+    },
+    collision_box = {{-0.15, -0.15}, {0.15, 0.15}},
+    selection_box = {{-0.4, -0.35}, {0.4, 0.45}},
+    pickup_position = {0, -2},
+    insert_position = {0, 2.2},
+    energy_per_movement = "5KJ",
+    energy_per_rotation = "5KJ",
+    rotation_speed = 0.02,
+    extension_speed = 0.0457,
+    hand_size = 1.5,
+    energy_source =
+    {
+      type = "heat",
+	  default_temperature = 15,
+      max_temperature = 5000,
+      min_working_temperature = 250,
+      minimum_glow_temperature = 100,
+      specific_heat = "1MJ",
+      max_transfer = "2GW",
+      connections =
+      {
+        {
+          position = {0, 0},
+          direction = defines.direction.north
+        },
+        {
+          position = {0, 0},
+          direction = defines.direction.east
+        },
+        {
+          position = {0, 0},
+          direction = defines.direction.south
+        },
+        {
+          position = {0, 0},
+          direction = defines.direction.west
+        },
+      },
+    },
+    fast_replaceable_group = "long-handed-inserter",
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    working_sound =
+    {
+      match_progress_to_activity = true,
+      sound =
+      {
+        {
+          filename = "__base__/sound/inserter-long-handed-1.ogg",
+          volume = 0.75
+        },
+        {
+          filename = "__base__/sound/inserter-long-handed-2.ogg",
+          volume = 0.75
+        },
+        {
+          filename = "__base__/sound/inserter-long-handed-3.ogg",
+          volume = 0.75
+        },
+        {
+          filename = "__base__/sound/inserter-long-handed-4.ogg",
+          volume = 0.75
+        },
+        {
+          filename = "__base__/sound/inserter-long-handed-5.ogg",
+          volume = 0.75
+        }
+      }
+    },
+    hand_base_picture =
+    {
+      filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-hand-base.png",
+      priority = "extra-high",
+      width = 8,
+      height = 34,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-hand-base.png",
+        priority = "extra-high",
+        width = 32,
+        height = 136,
+        scale = 0.25
+      }
+    },
+    hand_closed_picture =
+    {
+      filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-hand-closed.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-hand-closed.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    hand_open_picture =
+    {
+      filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-hand-open.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-hand-open.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    hand_base_shadow =
+    {
+      filename = "__base__/graphics/entity/burner-inserter/burner-inserter-hand-base-shadow.png",
+      priority = "extra-high",
+      width = 8,
+      height = 33,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/burner-inserter/hr-burner-inserter-hand-base-shadow.png",
+        priority = "extra-high",
+        width = 32,
+        height = 132,
+        scale = 0.25
+      }
+    },
+    hand_closed_shadow =
+    {
+      filename = "__base__/graphics/entity/burner-inserter/burner-inserter-hand-closed-shadow.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/burner-inserter/hr-burner-inserter-hand-closed-shadow.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    hand_open_shadow =
+    {
+      filename = "__base__/graphics/entity/burner-inserter/burner-inserter-hand-open-shadow.png",
+      priority = "extra-high",
+      width = 18,
+      height = 41,
+      hr_version =
+      {
+        filename = "__base__/graphics/entity/burner-inserter/hr-burner-inserter-hand-open-shadow.png",
+        priority = "extra-high",
+        width = 72,
+        height = 164,
+        scale = 0.25
+      }
+    },
+    platform_picture =
+    {
+      sheet =
+      {
+        filename = "__base__/graphics/entity/long-handed-inserter/long-handed-inserter-platform.png",
+        priority = "extra-high",
+        width = 46,
+        height = 46,
+        shift = {0.09375, 0},
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/long-handed-inserter/hr-long-handed-inserter-platform.png",
+          priority = "extra-high",
+          width = 105,
+          height = 79,
+          shift = util.by_pixel(1.5, 7.5-1),
+          scale = 0.5
+        }
+      }
+    },
+    circuit_wire_connection_points = circuit_connector_definitions["inserter"].points,
+    circuit_connector_sprites = circuit_connector_definitions["inserter"].sprites,
+    circuit_wire_max_distance = inserter_circuit_wire_max_distance,
+    default_stack_control_input_signal = inserter_default_stack_control_input_signal
+  },
+  {
     type = "item",
     name = "heated-inserter",
     icon = "__base__/graphics/icons/inserter.png",
@@ -208,6 +407,16 @@ data:extend(
     subgroup = dy.."inserter-2-heat",
     order = "heated-inserter",
     place_result = "heated-inserter",
+    stack_size = 200
+  },
+  {
+    type = "item",
+    name = "heated-long-inserter",
+    icon = "__base__/graphics/icons/long-handed-inserter.png",
+	flags = {},
+    subgroup = dy.."inserter-2-heat",
+    order = "heated-long-inserter",
+    place_result = "heated-long-inserter",
     stack_size = 200
   },
   {
@@ -221,6 +430,18 @@ data:extend(
 	  {"heat-pipe", 1},
 	},
     result = "heated-inserter"
+  },
+  {
+    type = "recipe",
+    name = "heated-long-inserter",
+    energy_required = 2.5,
+    enabled = false,
+    ingredients = {
+	  {"long-handed-inserter", 1}, 
+	  {"iron-plate", 6},
+	  {"heat-pipe", 1},
+	},
+    result = "heated-long-inserter"
   },
 }
 )

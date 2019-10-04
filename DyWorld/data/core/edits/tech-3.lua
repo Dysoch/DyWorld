81b3ba7323843f5f40ab270f8c5f211736f00bde
Dysoch <frits@devreugd.nl>
2019-10-04 20:52:59 +0200

    Heated Assembler

--- DyWorld/data/core/edits/tech-3.lua
+++ DyWorld/data/core/edits/tech-3.lua
@@ -133,7 +133,7 @@ local Tech_Recipe_Add = {
 	["farming-1"] = {"carrot", "boiler-water"},
 	["farming-2"] = {"wheat"},
 	["farming-4"] = {"wood-2"},
-	["nuclear-energy-1"] = {"heat-pipe", "heat-pipe-furnace", "heated-furnace", "heated-inserter", "heated-long-inserter"},
+	["nuclear-energy-1"] = {"heat-pipe", "heat-pipe-furnace", "heated-furnace", "heated-inserter", "heated-assembler", "heated-long-inserter"},
 } 
 
 for k,v in pairs(Tech_Recipe_Add) do

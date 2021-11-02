3a4bfbb9358707c06029e48d4531c9a53c344d01
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2021-11-02 22:09:58 +0100

    Condition for presence of transport drones mod
    Based on discord comment

--- DyComPa/data/tech/transport-drones.lua
+++ DyComPa/data/tech/transport-drones.lua
@@ -1,3 +1,5 @@
 
-DyW.Tech.Prereq.Override("transport-system", {"intermediates-1"})
-DyW.Tech.Prereq.Override("transport-depot-circuits", {"transport-system"})
+if mods["Transport_Drones"] then
+  DyW.Tech.Prereq.Override("transport-system", {"intermediates-1"})
+  DyW.Tech.Prereq.Override("transport-depot-circuits", {"transport-system"})
+end

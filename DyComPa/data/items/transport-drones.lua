ce9cf25c228d96bd58dba8005b1c10c8aeffd19a
OndrejMus <77000568+OndrejMus@users.noreply.github.com>
2021-11-02 23:18:53 +0100

    Condition for presence of transport drones mod

--- DyComPa/data/items/transport-drones.lua
+++ DyComPa/data/items/transport-drones.lua
@@ -1,18 +1,20 @@
-local change_item_group = {
-	["supply-depot"] = DyDs.."transport-drones",
-	["request-depot"] = DyDs.."transport-drones",
-  	["fuel-depot"] = DyDs.."transport-drones",
-  	["fluid-depot"] = DyDs.."transport-drones",
-  	["buffer-depot"] = DyDs.."transport-drones",
-  	["road-network-reader"] = DyDs.."transport-drones",
-  	["transport-depot-writer"] = DyDs.."transport-drones",
-	["transport-depot-reader"] = DyDs.."transport-drones",
-  	["transport-drone"] = DyDs.."transport-drones",
-	["road"] = DyDs.."transport-drones",  
-}
+if mods["Transport_Drones"] then
+	local change_item_group = {
+		["supply-depot"] = DyDs.."transport-drones",
+		["request-depot"] = DyDs.."transport-drones",
+		["fuel-depot"] = DyDs.."transport-drones",
+		["fluid-depot"] = DyDs.."transport-drones",
+		["buffer-depot"] = DyDs.."transport-drones",
+		["road-network-reader"] = DyDs.."transport-drones",
+		["transport-depot-writer"] = DyDs.."transport-drones",
+		["transport-depot-reader"] = DyDs.."transport-drones",
+		["transport-drone"] = DyDs.."transport-drones",
+		["road"] = DyDs.."transport-drones",  
+	}
 
-for k,v in pairs(change_item_group) do
-	if data.raw.item[k] then
-		data.raw.item[k].subgroup = v
+	for k,v in pairs(change_item_group) do
+		if data.raw.item[k] then
+			data.raw.item[k].subgroup = v
+		end
 	end
 end

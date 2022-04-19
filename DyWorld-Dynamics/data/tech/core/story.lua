adaf49c6e4a40868aea68c779f256a9b358d12ce
Dysoch <Dysoch@users.noreply.github.com>
2022-04-18 21:30:17 -0400

    Update story.lua

--- DyWorld-Dynamics/data/tech/core/story.lua
+++ DyWorld-Dynamics/data/tech/core/story.lua
@@ -21,10 +21,7 @@ for Act_k,Act_v in pairs(Story_Table_Base) do
 			},
             flags = {Story = true},
             prerequisites = {},
-            ingredients = {
-                {"dyworld-story-objectives-main-1", 1},
-                {"dyworld-story-objectives-side-1", 1},
-            },
+            ingredients = {},
             time = Dy_Tech_Time_Calc(1, true),
             order = "0",
         })
@@ -41,24 +38,34 @@ for Act_k,Act_v in pairs(Story_Table_Base) do
         elseif Act_k == 5 and Phase_k == 1 then
             data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].prerequisites = {"dyworld-story-"..(4).."-"..(1)}
         end
-        if Phase_v.objectives_side then
-            local Main_Amount = Story_tablelength(Phase_v.objectives)
-            local Side_Amount = 10 - Main_Amount
-            data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].ingredients = {
-                {"dyworld-story-objectives-main-"..Main_Amount, 1},
-                {"dyworld-story-objectives-side-"..Side_Amount, 1},
-            }
-        else
-            local Main_Amount = Story_tablelength(Phase_v.objectives)
-            data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].ingredients = {
-                {"dyworld-story-objectives-main-"..Main_Amount, 1},
-            }
-        end
         if Phase_v.unlocks then
             data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].effects = {}
             for k,v in pairs(Phase_v.unlocks) do
                 local Recipe = {type = "unlock-recipe", recipe = v}
-                table.insert(data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].effects, result)
+                table.insert(data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].effects, Recipe)
+            end
+        end
+    end
+end
+
+for Act_k,Act_v in pairs(Story_Table_Base) do
+    for Phase_k, Phase_v in pairs(Act_v) do
+        if Act_k == 1 and Phase_k == 1 then
+
+        else
+            if not Phase_v.objectives_side then
+                data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].ingredients = {}
+                local Main_Amount = Story_tablelength(Phase_v.objectives)
+                local Insert = {"dyworld-story-objectives-main-"..Main_Amount, 1}
+                table.insert(data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].ingredients, Insert)
+            else
+                data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].ingredients = {}
+                local Main_Amount = Story_tablelength(Phase_v.objectives)
+                local Side_Amount = 10 - Main_Amount
+                local Insert = {"dyworld-story-objectives-main-"..Main_Amount, 1}
+                table.insert(data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].ingredients, Insert)
+                local Insert = {"dyworld-story-objectives-side-"..Side_Amount, 1}
+                table.insert(data.raw.technology["dyworld-story-"..Act_k.."-"..Phase_k].ingredients, Insert)
             end
         end
     end

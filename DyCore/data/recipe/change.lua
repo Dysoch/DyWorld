1987f3db923fa9719838e3aa1c23c9e04698e0e8
Dysoch <Dysoch@users.noreply.github.com>
2022-08-18 18:56:10 +0200

    Added Recipe Category change script

--- DyCore/data/recipe/change.lua
+++ DyCore/data/recipe/change.lua
@@ -1,3 +1,12 @@
 
 
+DyW.Recipe.Change = {}
 
+function DyW.Recipe.Change.Category(RECIPE, CATEGORY)
+    if data.raw.recipe[RECIPE] then
+        if CATEGORY and data.raw["recipe-category"][CATEGORY] then
+            data.raw.recipe[RECIPE].category = CATEGORY
+        end
+    end
+    DyW.Fx.Edited("recipe", RECIPE, "Recipe_Change_Category")
+end

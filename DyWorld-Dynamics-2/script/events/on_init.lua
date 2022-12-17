449b787cff919a8f8ffe16bd07b545fda728191b
Dysoch <Dysoch@users.noreply.github.com>
2022-12-17 13:27:55 +0100

    time calc function

--- DyWorld-Dynamics-2/script/events/on_init.lua
+++ DyWorld-Dynamics-2/script/events/on_init.lua
@@ -3,6 +3,23 @@
 
 
 function Event_on_init()
-    
+    if not global.dyworld then
+        global.dyworld = {
+            players = {},
+            game = {},
+            surfaces = {},
+            time = {
+                actual = {
+                    tick = 0,
+                    second = 0,
+                    minute = 0,
+                    hour = 0,
+                    day = 0,
+                    year = 0,
+                },
+                log = "0:0:0:0:0",
+            },
+        }
+    end
 end
 
--- DyWorld-Dynamics-2/script/events/on_tick.lua
+++ DyWorld-Dynamics-2/script/events/on_tick.lua
@@ -3,5 +3,24 @@
 
 
 function Event_on_tick(event)
-
+    if event.tick%(60*1)==1 then
+        global.dyworld.time.actual.second = global.dyworld.time.actual.second + 1
+        if global.dyworld.time.actual.second >= 59 then
+            global.dyworld.time.actual.second = 0
+            global.dyworld.time.actual.minute = global.dyworld.time.actual.minute + 1
+        end
+        if global.dyworld.time.actual.minute >= 59 then
+            global.dyworld.time.actual.minute = 0
+            global.dyworld.time.actual.hour = global.dyworld.time.actual.hour + 1
+        end
+        if global.dyworld.time.actual.hour >= 23 then
+            global.dyworld.time.actual.hour = 0
+            global.dyworld.time.actual.day = global.dyworld.time.actual.day + 1
+        end
+        if global.dyworld.time.actual.day >= 364 then
+            global.dyworld.time.actual.day = 0
+            global.dyworld.time.actual.year = global.dyworld.time.actual.year + 1
+        end
+        global.dyworld.time.log = global.dyworld.time.actual.year..":"..global.dyworld.time.actual.day..":"..global.dyworld.time.actual.hour..":"..global.dyworld.time.actual.minute..":"..global.dyworld.time.actual.second
+    end
 end

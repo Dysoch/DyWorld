ca943679cb865c8082df5167342a36fada5fc02f
Dysoch <Dysoch@users.noreply.github.com>
2023-11-19 13:13:06 +0100

    bugfixes

--- DyWorld-Dynamics-2/data-final-fixes.lua
+++ DyWorld-Dynamics-2/data-final-fixes.lua
@@ -6,3 +6,9 @@ require("data.core.edits.trees")
 require("data.core.edits.rocks")
 require("data.core.edits.resources")
 require("data.core.edits.base-edits")
+
+if data.raw["utility-constants"]["default"].main_menu_simulations then
+    if data.raw["utility-constants"]["default"].main_menu_simulations.big_defense then
+        data.raw["utility-constants"]["default"].main_menu_simulations.big_defense = nil
+    end
+end

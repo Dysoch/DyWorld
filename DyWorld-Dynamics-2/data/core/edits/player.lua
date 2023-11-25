71dda5ec174b119642a0444437a81d392dcabeaf
Dysoch <Dysoch@users.noreply.github.com>
2023-11-25 22:35:37 +0100

    Update player.lua

--- DyWorld-Dynamics-2/data/core/edits/player.lua
+++ DyWorld-Dynamics-2/data/core/edits/player.lua
@@ -7,6 +7,7 @@ if data.raw.character.character then
     -- Player Edits --
     data.raw.character.character.max_health = 1000
     data.raw.character.character.crafting_categories = {DyDs.."assembling-1", "crafting"}
+    -- TODO add mining categories
     --data.raw.character.character.mining_categories = {"resource-solid-tier-0"}
     data.raw.character.character.inventory_size = 30
     data.raw.character.character.healing_per_tick = 0

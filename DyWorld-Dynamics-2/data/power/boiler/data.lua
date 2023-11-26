257e58ba86843eb4c3fcdeb04dd0203d5c753fb9
Dysoch <Dysoch@users.noreply.github.com>
2023-11-26 09:12:42 +0100

    Steam engines change

--- DyWorld-Dynamics-2/data/power/boiler/data.lua
+++ DyWorld-Dynamics-2/data/power/boiler/data.lua
@@ -3,8 +3,9 @@ if data.raw.boiler.boiler then
     data.raw.boiler.boiler.fluid_box.filter = nil
     data.raw.boiler.boiler.target_temperature = 250
     data.raw.boiler.boiler.next_upgrade = "boiler-2"
+    data.raw.boiler.boiler.energy_consumption = "1MW"
     data.raw.boiler.boiler.fast_replaceable_group = "energy"
-    data.raw.boiler.boiler.energy_source.effectivity = 8
+    data.raw.boiler.boiler.energy_source.effectivity = 1
     data.raw.boiler.boiler.energy_source.fuel_categories = {DyDs.."carbon"}
 end
 
@@ -15,16 +16,18 @@ local DyWorld_Prototype_2b = DyW.Fx.DC("item", "boiler", "boiler-3")
 
 
 DyWorld_Prototype_1a.target_temperature = 500
-DyWorld_Prototype_1a.energy_source.effectivity = 16
+DyWorld_Prototype_1a.energy_source.effectivity = 1
 DyWorld_Prototype_1a.energy_source.fuel_categories = {DyDs.."carbon", DyDs.."chemical"}
 DyWorld_Prototype_1a.next_upgrade = "boiler-3"
 DyWorld_Prototype_1a.fast_replaceable_group = "energy"
+DyWorld_Prototype_1a.energy_consumption = "2MW"
 
 DyWorld_Prototype_2a.target_temperature = 1000
-DyWorld_Prototype_2a.energy_source.effectivity = 32
+DyWorld_Prototype_2a.energy_source.effectivity = 1
 DyWorld_Prototype_2a.energy_source.fuel_categories = {DyDs.."carbon", DyDs.."chemical", DyDs.."oil"}
 DyWorld_Prototype_2a.next_upgrade = nil
 DyWorld_Prototype_2a.fast_replaceable_group = "energy"
+DyWorld_Prototype_2a.energy_consumption = "4MW"
 
 data:extend({
     DyWorld_Prototype_1a,
--- DyWorld-Dynamics-2/data/power/steam-engine/data.lua
+++ DyWorld-Dynamics-2/data/power/steam-engine/data.lua
@@ -1,8 +1,8 @@
 
 if data.raw.generator["steam-engine"] then
     data.raw.generator["steam-engine"].fluid_box.filter = nil
-    data.raw.generator["steam-engine"].fluid_usage_per_tick = (1 / 60)
-    data.raw.generator["steam-engine"].effectivity = 10
+    data.raw.generator["steam-engine"].fluid_usage_per_tick = (4 / 60)
+    data.raw.generator["steam-engine"].effectivity = 1
     data.raw.generator["steam-engine"].maximum_temperature = 250
     data.raw.generator["steam-engine"].next_upgrade = "steam-engine-2"
     data.raw.generator["steam-engine"].fast_replaceable_group = "energy"
@@ -14,19 +14,19 @@ local DyWorld_Prototype_1b = DyW.Fx.DC("item", "steam-engine", "steam-engine-2")
 local DyWorld_Prototype_2a = DyW.Fx.DC("generator", "steam-engine", "steam-engine-3")
 local DyWorld_Prototype_2b = DyW.Fx.DC("item", "steam-engine", "steam-engine-3")
 
-DyWorld_Prototype_1a.fluid_usage_per_tick = (2 / 60)
+DyWorld_Prototype_1a.fluid_usage_per_tick = (8 / 60)
 DyWorld_Prototype_1a.maximum_temperature = 500
-DyWorld_Prototype_1a.effectivity = 10
+DyWorld_Prototype_1a.effectivity = 1
 DyWorld_Prototype_1a.max_power_output = (1500000 / 60).."J" -- 1.5 MW
 DyWorld_Prototype_1a.next_upgrade = "steam-engine-3"
 DyWorld_Prototype_1a.fast_replaceable_group = "energy"
 
-DyWorld_Prototype_2a.fluid_usage_per_tick = (4 / 60)
+DyWorld_Prototype_2a.fluid_usage_per_tick = (16 / 60)
 DyWorld_Prototype_2a.maximum_temperature = 1000
-DyWorld_Prototype_2a.effectivity = 10
+DyWorld_Prototype_2a.effectivity = 1
 DyWorld_Prototype_2a.max_power_output = (4500000 / 60).."J" -- 4.5 MW
-DyWorld_Prototype_1a.next_upgrade = nil
-DyWorld_Prototype_1a.fast_replaceable_group = "energy"
+DyWorld_Prototype_2a.next_upgrade = nil
+DyWorld_Prototype_2a.fast_replaceable_group = "energy"
 
 data:extend({
     DyWorld_Prototype_1a,
